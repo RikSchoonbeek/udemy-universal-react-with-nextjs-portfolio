507ce8ad4a0489d9c0c6ce9f67424742645b8c17
rikschoonbeek <rik.schoonbeek@gmail.com>
2019-03-19 14:16:44 +0100

    1 - 11 Cleaner urls without query params

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,9 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 
-const PostLink = ({ title }) => (
+const PostLink = ({ slug, title }) => (
   <li>
-    <Link href={`/post?title=${title}`}>
+    <Link as={`/blog/${slug}`} href={`/post?title=${title}`}>
       <a>{title}</a>
     </Link>
   </li>
@@ -12,9 +12,9 @@ const PostLink = ({ title }) => (
 export default () => (
   <Layout title="My Blog">
     <ul>
-      <PostLink title="Angular" />
-      <PostLink title="React" />
-      <PostLink title="Vue" />
+      <PostLink slug="angular-post" title="Angular Post" />
+      <PostLink slug="react-post" title="React Post" />
+      <PostLink slug="vue-post" title="Vue Post" />
     </ul>
   </Layout>
 );
